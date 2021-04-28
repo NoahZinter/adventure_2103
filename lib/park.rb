3cdeed3acebe13eecfdaff3eae5a5c5808acfe33
NoahZinter <noah.zinter@gmail.com>
2021-04-28 09:09:36 -0600

    Test park exists

---
+++ lib/park.rb
@@ -0,0 +1,6 @@
+class Park
+
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ spec/park_spec.rb
@@ -0,0 +1,10 @@
+require './lib/trail'
+require './lib/park'
+
+describe Park do
+  describe '#initialize' do
+    it 'exists' do
+      park1 = Park.new('Capitol Reef')
+    end
+  end
+end
