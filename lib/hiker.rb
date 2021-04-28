e0487086b184419f710313c84b1226552bf80ea0
NoahZinter <noah.zinter@gmail.com>
2021-04-28 09:19:18 -0600

    Test hiker attributes

--- lib/hiker.rb
+++ lib/hiker.rb
@@ -1,4 +1,5 @@
 class Hiker
+  attr_reader :name, :experience_level
 
   def initialize(name, experience_level)
     @name = name
--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -6,5 +6,12 @@ describe Hiker do
       hiker = Hiker.new('Dora', :moderate)
       expect(hiker).is_a? Hiker
     end
+
+    it 'has attributes' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      expect(hiker.name).to eq 'Dora'
+      expect(hiker.experience_level).to eq :moderate
+    end
   end
 end
