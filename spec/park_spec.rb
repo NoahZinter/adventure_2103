1f62e642f7992241273e3fb0704e24279cc81f2e
NoahZinter <noah.zinter@gmail.com>
2021-04-28 09:11:50 -0600

    Add missing expectations

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -5,10 +5,14 @@ describe Park do
   describe '#initialize' do
     it 'exists' do
       park1 = Park.new('Capitol Reef')
+
+      expect(park1).is_a? Park
     end
 
     it 'has a name' do
       park1 = Park.new('Capitol Reef')
+
+      expect(park1.name).to eq 'Capitol Reef'
     end
   end
 end
